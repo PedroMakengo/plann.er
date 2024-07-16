9e6835ac0e521ecb0f477680bedfa638a67255e5
PedroMakengo <pedromakengomiguel@gmail.com>
2024-07-16 21:44:40 +0100

    Adicionado o formulário de confirmar

--- src/app.tsx
+++ src/app.tsx
@@ -206,6 +206,54 @@ export function App() {
           </div>
         </div>
       )}
+
+      <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+        <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+          <div className="space-y-2">
+            <div className="flex items-center justify-between">
+              <h2 className="text-lg font-semibold">
+                Confirmar a criação de viagem
+              </h2>
+              <button onClick={closeGuestsModal}>
+                <X className="size-5  text-zinc-400" />
+              </button>
+            </div>
+            <p className="text-sm text-zinc-400">
+              Para concluir a criação de viagem para{' '}
+              <span className="font-semibold text-zinc-100">
+                Florianópolis , Brasil
+              </span>{' '}
+              nas datas de
+              <span className="font-semibold text-zinc-100">
+                16 a 27 de Agosto
+              </span>
+              de 2024 preencha seus dados abaixo:
+            </p>
+          </div>
+
+          <form
+            onSubmit={addNewEmailToInvite}
+            className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2"
+          >
+            <div className="px-2 flex flex-1 items-center justify-center gap-2">
+              <AtSign className="size-5 text-zinc-400" />
+              <input
+                type="email"
+                name="email"
+                placeholder="Digite o e-mail do convidado"
+                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+            >
+              Convidar
+              <Plus className="size-5" />
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
